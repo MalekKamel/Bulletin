61756c158157e437b9151385291f00050ed14945
ShabanKamel <sh3ban.kamel@gmail.com>
2019-12-27 21:43:41 +0400

    improve code

--- lib/src/main/java/com/sha/bulletin/DisplayUtil.kt
+++ lib/src/main/java/com/sha/bulletin/DisplayUtil.kt
@@ -3,7 +3,6 @@ package com.sha.bulletin
 import android.content.Context
 
 object DisplayUtil {
-
     fun dp2px(context: Context, dpValue: Float): Int {
         val scale = context.resources.displayMetrics.density
         return (dpValue * scale + 0.5f).toInt()
