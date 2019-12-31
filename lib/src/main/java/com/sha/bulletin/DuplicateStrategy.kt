a2f95519cf0edc9e494a5e9da4953a4b802fc9f2
ShabanKamel <sh3ban.kamel@gmail.com>
2020-01-01 00:51:12 +0400

    document DuplicateStrategy

--- lib/src/main/java/com/sha/bulletin/DuplicateStrategy.kt
+++ lib/src/main/java/com/sha/bulletin/DuplicateStrategy.kt
@@ -1,31 +1,54 @@
 package com.sha.bulletin
 
+/**
+ * A strategy used to manage duplicate bulletins behavior.
+ */
 interface DuplicateStrategy {
     fun shouldIgnore(bulletin: Bulletin, displayedBulletins: Set<Bulletin>): Boolean
 }
 
+/**
+ * This strategy allows all bulletins to be duplicated.
+ */
 class DefaultDuplicateStrategy: DuplicateStrategy {
     override fun shouldIgnore(bulletin: Bulletin, displayedBulletins: Set<Bulletin>) = false
 }
 
+/**
+ * This strategy prevents duplicating the [Bulletin] if there's another [Bulletin]
+ * with the same name displayed.
+ */
 class NameDuplicateStrategy: DuplicateStrategy {
     override fun shouldIgnore(bulletin: Bulletin, displayedBulletins: Set<Bulletin>): Boolean {
         return isAnyDisplayed(bulletin.name)
     }
 }
 
+/**
+ * This strategy prevents duplicating the [Bulletin] if there's another [Bulletin]
+ * with the same content displayed.
+ */
 class ContentDuplicateStrategy: DuplicateStrategy {
     override fun shouldIgnore(bulletin: Bulletin, displayedBulletins: Set<Bulletin>): Boolean {
         return isAnyDisplayedWithContent(bulletin.content)
     }
 }
 
+/**
+ * This strategy prevents duplicating the [Bulletin] if there's another [Bulletin]
+ * with the same name & content displayed.
+ */
 class NameContentDuplicateStrategy: DuplicateStrategy {
     override fun shouldIgnore(bulletin: Bulletin, displayedBulletins: Set<Bulletin>): Boolean {
         return isAnyDisplayed(bulletin.name, bulletin.content)
     }
 }
 
+/**
+ * This strategy allows displaying the [Bulletin] only if there're no any bulletins
+ * displayed. In another words, it allows a single [Bulletin] to be displayed at a time
+ * and ignores any other bulletins.
+ */
 class SingleDuplicateStrategy: DuplicateStrategy {
     override fun shouldIgnore(bulletin: Bulletin, displayedBulletins: Set<Bulletin>) = isAnyDisplayed()
 }
